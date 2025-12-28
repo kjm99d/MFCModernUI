981be920a33c1b5a58f9e4a1e749df5497d10f72
Jungmin Kim <kjm99d@naver.com>
2025-12-29 04:36:29 +0900

    feat: MFC Modern UI 라이브러리 프로젝트 초기화
    Visual Studio MFC 정적 라이브러리 프로젝트 구조:
    - 기본 MFC 초기화 코드

---
+++ MFCModernUI/MFCModernUI.cpp
@@ -0,0 +1,51 @@
+﻿// MFCModernUI.cpp : 정적 라이브러리를 위한 함수를 정의합니다.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "MFCModernUI.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// 유일한 애플리케이션 개체입니다.
+
+CWinApp theApp;
+
+using namespace std;
+
+int main()
+{
+    int nRetCode = 0;
+
+    HMODULE hModule = ::GetModuleHandle(nullptr);
+
+    if (hModule != nullptr)
+    {
+        // MFC를 초기화합니다. 초기화하지 못한 경우 오류를 인쇄합니다.
+        if (!AfxWinInit(hModule, nullptr, ::GetCommandLine(), 0))
+        {
+            // TODO: 여기에 애플리케이션 동작을 코딩합니다.
+            wprintf(L"심각한 오류: MFC 초기화 실패\n");
+            nRetCode = 1;
+        }
+        else
+        {
+            // TODO: 여기에 애플리케이션 동작을 코딩합니다.
+        }
+    }
+    else
+    {
+        // TODO: 오류 코드를 필요에 따라 수정합니다.
+        wprintf(L"심각한 오류: GetModuleHandle 실패\n");
+        nRetCode = 1;
+    }
+
+    return nRetCode;
+}
+// TODO: 라이브러리 함수의 예제입니다.
+void fnMFCModernUI()
+{
+}
---
+++ MFCModernUI/framework.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+#include "targetver.h"
+#define WIN32_LEAN_AND_MEAN             // 거의 사용되지 않는 내용을 Windows 헤더에서 제외합니다.
+#define _ATL_CSTRING_EXPLICIT_CONSTRUCTORS      // 일부 CString 생성자는 명시적으로 선언됩니다.
+#define _AFX_NO_MFC_CONTROLS_IN_DIALOGS         // 대화 상자에서 MFC 컨트롤에 대한 지원을 제거합니다.
+
+#ifndef VC_EXTRALEAN
+#define VC_EXTRALEAN            // 거의 사용되지 않는 내용을 Windows 헤더에서 제외합니다.
+#endif
+
+#include <afx.h>
+#include <afxwin.h>         // MFC 핵심 및 표준 구성 요소입니다.
